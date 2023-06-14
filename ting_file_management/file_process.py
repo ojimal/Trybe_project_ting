75e58da00c089ab01a688165abfde16f2e5475cd
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-06-14 21:41:54 +0200

    feat add process and remove functions to file_process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,27 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for i in range(0, len(instance)):
+        if instance.search(i)['nome_do_arquivo'] == path_file:
+            return None
+    file_lines = txt_importer(path_file)
+    out = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(file_lines),
+        'linhas_do_arquivo': file_lines,
+    }
+    instance.enqueue(out)
+    print(out, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        print('Não há elementos', file=sys.stdout)
+        return None
+    file_removed = instance.dequeue()['nome_do_arquivo']
+    print(f'Arquivo {file_removed} removido com sucesso', file=sys.stdout)
 
 
 def file_metadata(instance, position):
