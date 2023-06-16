bf3cb79cc5a471d2d68322565313ba04d0e43bee
ojimaluis <106823349+ojimaluis@users.noreply.github.com>
2023-06-16 13:02:06 +0200

    feat add def exists_word in word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,21 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    occurrences = []
+
+    for i in range(len(instance)):
+        file_data = instance.search(i)
+        for line_index, line in enumerate(file_data["linhas_do_arquivo"]):
+            if word.casefold() in line.casefold():
+                occurrences.append({"linha": line_index + 1})
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file_data["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            })
+        occurrences = []
+
+    return result
 
 
 def search_by_word(word, instance):
